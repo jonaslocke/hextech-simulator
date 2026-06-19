4b9eea8d45ab914115ac675407436da38f04a75e
Jonas Antunes <jonaslocke@gmail.com>
2026-06-19 18:47:42 -0300

    Remove failed card catalog admin attempt

--- src/app/admin/card-catalog/page.tsx
+++
@@ -1,6 +0,0 @@
-import { CardCatalogAdminWorkbench } from "@/features/card-catalog-admin/components/card-catalog-admin-workbench";
-
-export default function CardCatalogAdminPage() {
-  return <CardCatalogAdminWorkbench />;
-}
-
--- src/server/card-catalog-admin/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./behavior-analysis";
-export * from "./catalog-ingestion";
-export * from "./identity";
-export * from "./repositories";
-export * from "./runtime-behavior";
-export * from "./source-parser";
-export * from "./types";
-
