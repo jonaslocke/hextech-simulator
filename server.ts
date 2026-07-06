6269c0d71c87a86240cb322493d43455d34774eb
Jonas Antunes <jonaslocke@gmail.com>
2026-07-06 13:55:29 -0300

    server config to work on render

--- server.ts
+++ server.ts
@@ -8,18 +8,26 @@ import {
 } from "./src/server/online-matchmaking";
 
 const dev = process.argv.includes("--dev");
-const hostname = process.env.HOSTNAME ?? "0.0.0.0";
+const hostname = "0.0.0.0";
 const port = Number(process.env.PORT ?? 3000);
+
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
 await app.prepare();
 
 const httpServer = createServer(handler);
+
+const socketCorsOrigin =
+  process.env.SOCKET_CORS_ORIGIN ??
+  (dev ? `http://localhost:${port}` : undefined);
+
 const io = new Server(httpServer, {
-  cors: {
-    origin: process.env.SOCKET_CORS_ORIGIN ?? `http://localhost:${port}`,
-  },
+  cors: socketCorsOrigin
+    ? {
+        origin: socketCorsOrigin,
+      }
+    : undefined,
 });
 
 const roomService = new OnlineRoomService(new OnlineRoomRegistry());
